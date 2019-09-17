e1e8b2ce6eba2f5542255604a1b83ea37f46ef98
Waldemar Leontiev <waldemar.leontiev@gmail.com>
2019-09-17 12:54:46 +0300

    Initial Commit

---
+++ WatchDemo WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchDemo WatchKit Extension
+//
+//  Created by Waldemar on 17/09/2019.
+//  Copyright © 2019 waldemarleo. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
